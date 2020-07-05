620c7df9eaf5387e791aa25d7fbcea33f2fd1062
Summit07 <summitgupta07@gmail.com>
2020-07-05 15:18:19 +0530

    getup

---
+++ public/javascript/fileUpload.js
@@ -0,0 +1,12 @@
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageResize,
+  FilePondPluginImageFileEncode
+);
+
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetHeight: 150,
+});
+FilePond.parse(document.body);
